3f22ca99b13af4331f43c448dafca7e88e721298
Nikhil Gohil <nikhil.gohil3@gmail.com>
2021-03-11 05:43:45 +0000

    z2_plus: Stag-ify

--- stag_z2_plus.mk
+++ stag_z2_plus.mk
@@ -20,20 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common PixysOS stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
-
-# Pixys stuff
-DEVICE_MAINTAINERS="Aswin A S & Nikhil Gohil"
-
-# GApps
-TARGET_GAPPS_ARCH := arm64
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common StagOS stuff.
+$(call inherit-product, vendor/stag/main.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_z2_plus
+PRODUCT_NAME := stag_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_BRAND := ZUK
 PRODUCT_MODEL := Z2 Plus
